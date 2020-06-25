bc2bfb5744f1bda0d484fb59aad0177fbd72e5dc
koshifu <52692335+koshifu@users.noreply.github.com>
2020-06-25 17:55:52 +1000

    ScoringAndLootBoxAnimations

---
+++ CarnivalVR/Assets/Scripts/ShowHideUI.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowHideUI : MonoBehaviour
+{
+    public GameObject ui;
+
+    public void OnMouseUpAsButton()
+    {
+        ui.SetActive(!ui.activeSelf);
+    }
+}
---
+++ CarnivalVR/Assets/Scripts/Testing/LootboxAnimTesting.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LootboxAnimTesting : MonoBehaviour
+{
+    public Animation anim;
+
+    private void OnMouseUpAsButton()
+    {
+        anim.Play("OpenLootBox");
+    }
+}
